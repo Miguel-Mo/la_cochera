21f186d3c3c15323053279da2ffbdb4aef631e46
=Jesús Cañas <jesus.c.cobos@gmail.com>
2020-10-27 08:59:28 +0100

    Cambio del avatar de png a svg y ruta del login

--- src/main/java/Cochera/Main.java
+++ src/main/java/Cochera/Main.java
@@ -9,8 +9,8 @@ import javafx.stage.Stage;
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("/login.fxml"));
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/Login/login.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 600, 400));
         primaryStage.show();

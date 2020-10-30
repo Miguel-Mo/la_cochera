3c03d95f1aa97642318f1173b0f44985c3d5cf53
=Jesús Cañas <jesus.c.cobos@gmail.com>
2020-10-30 13:36:34 +0100

    Se ajusta el borde y cuando se pulsa enter enviamos el formulario de login

--- src/main/java/Cochera/Controllers/ControladorLogin.java
+++ src/main/java/Cochera/Controllers/ControladorLogin.java
@@ -5,9 +5,12 @@ import Cochera.models.Usuario.Usuario;
 import Cochera.utils.Vistas.VentanaCustom;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -19,6 +22,8 @@ public class ControladorLogin extends VentanaCustom {
     @FXML
     private PasswordField campoPass;
     @FXML
+    private Button login;
+    @FXML
     private Label error;
 
     //Constructor. Es lo primero que se realiza. Antes que initialize()
@@ -28,6 +33,14 @@ public class ControladorLogin extends VentanaCustom {
     //Inicializa después de que se haya cargado el fxml (la vista) a la que está conectado
     private void initialize() {
         super.moverVentana();
+
+        // Para que al clicar Enter en el teclado se mande el formulario
+        parent.addEventHandler(KeyEvent.KEY_PRESSED, event -> {
+            if (event.getCode() == KeyCode.ENTER) {
+                login.fire();
+                event.consume();
+            }
+        });
     }
 
     @FXML

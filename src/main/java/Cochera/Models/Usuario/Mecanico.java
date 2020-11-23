954d125c00e21eec03521dc3c3a785497c88a5a6
Miguel <miguel.moreno@dataleanmakers.es>
2020-11-23 22:41:36 +0100

    Usuario Mecanico

--- src/main/java/Cochera/Models/Usuario/Mecanico.java
+++ src/main/java/Cochera/Models/Usuario/Mecanico.java
@@ -1,12 +1,21 @@
 package Cochera.Models.Usuario;
 
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.beans.property.StringProperty;
+
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class Mecanico extends Usuario {
 
-
+    private BooleanProperty esJefe;
+    private int usuarioID;
     public Mecanico(ResultSet datos) throws SQLException {
         super(datos);
+
+        esJefe =new SimpleBooleanProperty(datos.getBoolean("esJefe"));
+        usuarioID = datos.getInt("usuarioID");
+
     }
 }

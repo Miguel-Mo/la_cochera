f857db9aebf2396efdf0830b077fad424f300874
Jesús Cañas <jesus.c.cobos@gmail.com>
2020-11-26 07:01:19 +0100

    Comprobaciones en el formulario por partes de reparaciones

--- src/main/java/Cochera/Controllers/Mecanicos/CModalReparacionNueva.java
+++ src/main/java/Cochera/Controllers/Mecanicos/CModalReparacionNueva.java
@@ -35,35 +35,54 @@ import java.util.prefs.Preferences;
 public class CModalReparacionNueva extends CModal {
 
 
-    @FXML private Boton btnAccion;
-    @FXML private Boton btnAtras;
-    @FXML private Boton btnAdelante;
+    @FXML
+    private Boton btnAccion;
+    @FXML
+    private Boton btnAtras;
+    @FXML
+    private Boton btnAdelante;
 
-    @FXML private Pane seccion1;
-    @FXML private TextField tfNombre;
-    @FXML private TextField tfEmail;
-    @FXML private TextField tfApellidos;
-    @FXML private TextField tfTelefono;
+    @FXML
+    private Pane seccion1;
+    @FXML
+    private TextField tfNombre;
+    @FXML
+    private TextField tfEmail;
+    @FXML
+    private TextField tfApellidos;
+    @FXML
+    private TextField tfTelefono;
 
-    @FXML private Pane seccion2;
-    @FXML private TextField marcaVehiculo;
-    @FXML private TextField modeloVehiculo;
-    @FXML private TextField matricula;
-    @FXML private ComboBox<TipoVehiculo> cbTipoVehiculo;
+    @FXML
+    private Pane seccion2;
+    @FXML
+    private TextField marcaVehiculo;
+    @FXML
+    private TextField modeloVehiculo;
+    @FXML
+    private TextField matricula;
+    @FXML
+    private ComboBox<TipoVehiculo> cbTipoVehiculo;
 
-    @FXML private Pane seccion3;
-    @FXML private SearchableComboBox<Mecanico> cbMecanico;
-    @FXML private TextField tfPresupuestoEstimado;
-    @FXML private TextField tfTiempoEstimado;
-    @FXML private TextArea taProblema;
-    @FXML private TextArea taComponentes;
+    @FXML
+    private Pane seccion3;
+    @FXML
+    private SearchableComboBox<Mecanico> cbMecanico;
+    @FXML
+    private TextField tfPresupuestoEstimado;
+    @FXML
+    private TextField tfTiempoEstimado;
+    @FXML
+    private TextArea taProblema;
+    @FXML
+    private TextArea taComponentes;
 
     private int numSeccion;
     private Pane[] secciones;
     private FilteredList<Reparacion> listaFiltrable;
     private FilteredList<Mecanico> mecanicos;
 
-    private final int concesionarioID = Integer.parseInt(Preferences.userRoot().get("concesionarioID",null));
+    private final int concesionarioID = Integer.parseInt(Preferences.userRoot().get("concesionarioID", null));
 
 
     public CModalReparacionNueva() {
@@ -76,14 +95,14 @@ public class CModalReparacionNueva extends CModal {
 
         secciones = new Pane[]{seccion1, seccion2, seccion3};
 
-        btnAtras.establecerImagen(new ImageView("/icons/left.png"),50);
+        btnAtras.establecerImagen(new ImageView("/icons/left.png"), 50);
         btnAtras.setOnAction(this::cambiarSeccion);
         btnAtras.setVisible(false);
 
-        btnAdelante.establecerImagen(new ImageView("/icons/right.png"),50);
+        btnAdelante.establecerImagen(new ImageView("/icons/right.png"), 50);
         btnAdelante.setOnAction(this::cambiarSeccion);
 
-        btnAccion.establecerImagen(new ImageView("/icons/icons8-checked-64.png"),50);
+        btnAccion.establecerImagen(new ImageView("/icons/icons8-checked-64.png"), 50);
         btnAccion.setOnAction(this::crear);
         btnAccion.setVisible(false);
 
@@ -113,18 +132,18 @@ public class CModalReparacionNueva extends CModal {
     }
 
     @FXML
-    private void cambiarSeccion(ActionEvent e)  {
-        if (!checkSeccion()) return;
-
+    private void cambiarSeccion(ActionEvent e) {
         Button botonPulsado = (Button) e.getSource();
 
         if (numSeccion == 1 && botonPulsado.equals(btnAdelante)) {
+            if (!checkSeccion()) return;
             numSeccion++;
             btnAtras.setVisible(true);
-        }else if (numSeccion == 2 && botonPulsado.equals(btnAtras)) {
+        } else if (numSeccion == 2 && botonPulsado.equals(btnAtras)) {
             numSeccion--;
             btnAtras.setVisible(false);
         } else if (numSeccion == 2 && botonPulsado.equals(btnAdelante)) {
+            if (!checkSeccion()) return;
             numSeccion++;
             btnAdelante.setVisible(false);
             btnAccion.setVisible(true);
@@ -194,29 +213,114 @@ public class CModalReparacionNueva extends CModal {
             throwables.printStackTrace();
         }
 
-        ((ObservableList<Reparacion>)listaFiltrable.getSource()).add(reparacion);
+        ((ObservableList<Reparacion>) listaFiltrable.getSource()).add(reparacion);
 
         btnCancelar.fire();
     }
 
     private boolean checkSeccion() {
+        resetError();
         boolean resultado = true;
 
         switch (numSeccion) {
             case 1:
 
+                if (tfNombre.getText().trim().length() == 0) {
+                    resultado = false;
+                    tfNombre.setStyle("-fx-border-color: RED");
+                }
+
+                if (tfEmail.getText().trim().length() == 0) {
+                    resultado = false;
+                    tfEmail.setStyle("-fx-border-color: RED");
+                }
+
+                if (tfApellidos.getText().trim().length() == 0) {
+                    resultado = false;
+                    tfApellidos.setStyle("-fx-border-color: RED");
+                }
+
+                if (tfTelefono.getText().trim().length() == 0) {
+                    resultado = false;
+                    tfTelefono.setStyle("-fx-border-color: RED");
+                }
+
                 break;
 
             case 2:
+
+                if (marcaVehiculo.getText().trim().length() == 0) {
+                    resultado = false;
+                    marcaVehiculo.setStyle("-fx-border-color: RED");
+                }
+
+                if (modeloVehiculo.getText().trim().length() == 0) {
+                    resultado = false;
+                    modeloVehiculo.setStyle("-fx-border-color: RED");
+                }
+
+                if (matricula.getText().trim().length() == 0) {
+                    resultado = false;
+                    matricula.setStyle("-fx-border-color: RED");
+                }
+
+                if (cbTipoVehiculo.getValue() == null) {
+                    resultado = false;
+                    cbTipoVehiculo.setStyle("-fx-border-color: RED");
+                }
 
                 break;
 
             case 3:
 
+                if (cbMecanico.getValue() == null) {
+                    resultado = false;
+                    cbMecanico.setStyle("-fx-border-color: RED");
+                }
+
+                if (tfPresupuestoEstimado.getText().trim().length() == 0 ||
+                        !tfPresupuestoEstimado.getText().matches("^[0-9]*$")) {
+                    resultado = false;
+                    tfPresupuestoEstimado.setStyle("-fx-border-color: RED");
+                }
+
+                if (tfTiempoEstimado.getText().trim().length() == 0 ||
+                        !tfTiempoEstimado.getText().matches("^(0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$")) {
+                    resultado = false;
+                    tfTiempoEstimado.setStyle("-fx-border-color: RED");
+                }
+
+                if (taProblema.getText().trim().length() == 0) {
+                    resultado = false;
+                    taProblema.setStyle("-fx-border-color: RED");
+                }
+
+                if (taComponentes.getText().trim().length() == 0) {
+                    resultado = false;
+                    taComponentes.setStyle("-fx-border-color: RED");
+                }
+
                 break;
         }
 
         return resultado;
+    }
+
+    private void resetError() {
+
+        tfNombre.setStyle("-fx-border-color: transparent");
+        tfEmail.setStyle("-fx-border-color: transparent");
+        tfApellidos.setStyle("-fx-border-color: transparent");
+        tfTelefono.setStyle("-fx-border-color: transparent");
+        modeloVehiculo.setStyle("-fx-border-color: transparent");
+        matricula.setStyle("-fx-border-color: transparent");
+        cbTipoVehiculo.setStyle("-fx-border-color: transparent");
+        cbMecanico.setStyle("-fx-border-color: transparent");
+        tfPresupuestoEstimado.setStyle("-fx-border-color: transparent");
+        tfTiempoEstimado.setStyle("-fx-border-color: transparent");
+        taProblema.setStyle("-fx-border-color: transparent");
+        taComponentes.setStyle("-fx-border-color: transparent");
+
     }
 
 
